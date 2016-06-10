928d2ab473ee306249eb16004adcf63d860c4b55
Miguel Augusto Aliaga Pantoja <maliaga.pantoja@gmail.com>
2016-06-10 00:06:44 -0500

    add: django cors

--- mankiapp/settings.py
+++ mankiapp/settings.py
@@ -45,11 +45,15 @@ INSTALLED_APPS = [
     'benefit.apps.BenefitConfig',
     'whishlist.apps.WhishlistConfig',
     'recluit.apps.RecluitConfig',
+
+    'corsheaders',
+
 ]
 
 MIDDLEWARE_CLASSES = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -57,6 +61,7 @@ MIDDLEWARE_CLASSES = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+CORS_ORIGIN_ALLOW_ALL=True
 
 ROOT_URLCONF = 'mankiapp.urls'
 
